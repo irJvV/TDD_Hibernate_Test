b080852315360bac5a511d5bc2c3e4d3300bfb8e
irJvV <gpabna@icloud.com>
2014-03-19 16:04:37 +0100

    Aanpassen en Fine-Tunen Hibernate koppeling -- opzetten en beperken MySQL privileges voor Test Wachtwoord en Password --- TDD_Test ---

--- src/gpa/hibernate/main/Hibernate_Entry.java
+++ src/gpa/hibernate/main/Hibernate_Entry.java
@@ -1,10 +1,7 @@
 package gpa.hibernate.main;
 
 public class Hibernate_Entry {
-	public static void main(String[] args) {
-		System.out.println("*********** Welcome Hibernate Test ***********");
-	}
-
+	
 	public static String Welcome() {
 		return "Entry";
 	}
